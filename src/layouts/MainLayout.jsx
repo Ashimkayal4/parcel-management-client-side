9cde5680f44bea4df037951797d13e0b8ed81902
Ashimkayal4 <ashimkayal4@gmail.com>
2025-01-13 22:55:02 +0600

    added basic route and start the project

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <section className="w-11/12 mx-auto">
+                <Navbar></Navbar>
+                <Outlet></Outlet>
+            </section>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainLayout;
